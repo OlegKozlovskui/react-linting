198ac8c3aaf767b8133856766dae3973971ca5b5
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-11-22 12:45:27 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const state = {
+  name: "Oleg"
+};
 
 function App() {
+  const { name } = state;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        sssssssssssssssssssssssssssssssss
+        sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        sssssssssssss
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
